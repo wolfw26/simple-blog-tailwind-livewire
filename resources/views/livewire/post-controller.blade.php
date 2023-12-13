4d75af9a425f3a8f8c2fb0b4961fa3f1b8db6713
whitewolf <wolfw1926@gmail.com>
2023-12-14 01:41:53 +0800

    Home style "Main Konten card"

--- resources/views/livewire/post-controller.blade.php
+++ resources/views/livewire/post-controller.blade.php
@@ -7,50 +7,112 @@
         </div>
     </div>
     <div class="w-full py-2">
-        <div class="flex flex-col">
-            {{-- Blog Konten Card --}}
-            <div class=" max-w-screen flex px-1 border-b py-2 border-b-slate-300">
-                <div class=" max-w-[40%] h-40 rounded-md">
-                    <img class=" min-h-full rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+        <div class="flex flex-col sm:grid sm:grid-cols-4 sm:gap-3 sm:mt-5">
+            {{-- Blog Konten Card main --}}
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300 sm:col-span-4">
+                <div class=" max-w-[40%] sm:max-w-[75%] h-40 sm:min-h-90 sm:h-60 sm:ml-[15%] sm:shadow-lg rounded-md">
+                    <img class=" min-h-full  sm:max-h-[90%] sm:w-full  rounded-lg  sm:hover:z-20 delay-150 duration-700 ease-in-out sm:hover:translate-y-3 sm:hover:skew-x-3 sm:hover:ring-2 sm:hover:ring-blue-600 " src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
                 </div>
-                <div class="w-full ml-3  px-1 py-1 relative">
+                <div class="w-full ml-3  px-1 py-1 relative sm:text-center">
                     <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <span class="absolute bottom-1 mt-2 text-slate-500">Jan 12. 8 minute ago</span>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
                 </div>
             </div>
             {{-- /blog Konten Card --}}
             {{-- Blog Konten Card --}}
-            <div class=" max-w-screen flex px-1 border-b py-2 border-b-slate-300">
-                <div class=" max-w-[40%] h-40 rounded-md">
-                    <img class=" min-h-full rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
                 </div>
                 <div class="w-full ml-3  px-1 py-1 relative">
                     <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <span class="absolute bottom-1 mt-2 text-slate-500">Jan 12. 8 minute ago</span>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
                 </div>
             </div>
             {{-- /blog Konten Card --}}
             {{-- Blog Konten Card --}}
-            <div class=" max-w-screen flex px-1 border-b py-2 border-b-slate-300">
-                <div class=" max-w-[40%] h-40 rounded-md">
-                    <img class=" min-h-full rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
                 </div>
                 <div class="w-full ml-3  px-1 py-1 relative">
                     <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <span class="absolute bottom-1 mt-2 text-slate-500">Jan 12. 8 minute ago</span>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
                 </div>
             </div>
             {{-- /blog Konten Card --}}
             {{-- Blog Konten Card --}}
-            <div class=" max-w-screen flex px-1 border-b py-2 border-b-slate-300">
-                <div class=" max-w-[40%] h-40 rounded-md">
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+                </div>
+                <div class="w-full ml-3  px-1 py-1 relative">
+                    <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
+                </div>
+            </div>
+            {{-- /blog Konten Card --}}
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+                </div>
+                <div class="w-full ml-3  px-1 py-1 relative">
+                    <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
+                </div>
+            </div>
+            {{-- /blog Konten Card --}}
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+                </div>
+                <div class="w-full ml-3  px-1 py-1 relative">
+                    <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
+                </div>
+            </div>
+            {{-- /blog Konten Card --}}
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+                </div>
+                <div class="w-full ml-3  px-1 py-1 relative">
+                    <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
+                </div>
+            </div>
+            {{-- /blog Konten Card --}}
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+                </div>
+                <div class="w-full ml-3  px-1 py-1 relative">
+                    <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
+                </div>
+            </div>
+            {{-- /blog Konten Card --}}
+            <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
+                    <img class=" min-h-full sm:h-3/5 sm:w-full  rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
+                </div>
+                <div class="w-full ml-3  px-1 py-1 relative">
+                    <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
+                </div>
+            </div>
+            {{-- /blog Konten Card --}}
+            {{-- /blog Konten Card --}}
+            {{-- Blog Konten Card --}}
+            {{-- <div class=" max-w-screen flex sm:flex-col px-1 border-b py-2 border-b-slate-300">
+                <div class=" max-w-[40%] sm:max-w-full h-40 rounded-md">
                     <img class=" min-h-full rounded-lg" src="{{ asset('assets/img/library.jpg') }}" alt="" srcset="">
                 </div>
                 <div class="w-full ml-3  px-1 py-1 relative">
                     <h3 class=" text-lg font-merriweather font-extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <span class="absolute bottom-1 mt-2 text-slate-500">Jan 12. 8 minute ago</span>
+                    <span class="absolute bottom-1 mt-2 text-slate-500 sm:static">Jan 12. 8 minute ago</span>
                 </div>
-            </div>
+            </div> --}}
             {{-- /blog Konten Card --}}
         </div>
     </div>
